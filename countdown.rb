21ea4628404fe82c143e4874aa7aaaa298216774
elsa chen <snowing222@gmail.com>
2020-05-15 20:35:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,18 @@
 #write your code here
 
 def countdown(num)
+
+  
   until num==0
+    
     puts "#{num} SECOND(S)!"
     num-=1
-    countdown_with_sleep
+    countdown_with_sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
-  return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep 1
+def countdown_with_sleep(sec)
+  sleep sec
 
 end
