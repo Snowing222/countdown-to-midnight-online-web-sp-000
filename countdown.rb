fd3262a0f0be2ce2a4a81ab8d31c2364f4a5ad94
elsa chen <snowing222@gmail.com>
2020-05-15 20:32:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,12 @@
 
 def countdown(num)
   until num==0
-    puts "#{num}SECOND(S)!"
+    puts "#{num} SECOND(S)!"
     num-=1
     countdown_with_sleep
   end
   puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
